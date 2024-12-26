8282e08f0de694de6c053cceecc0acd2e329e94f
JosephCCN <joseph02252003@gmail.com>
2024-12-27 00:33:00 +0800

    docker

--- judge/judge.go
+++ judge/judge.go
@@ -1,14 +1,40 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os/exec"
 	"path/filepath"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+func connect(uri string) (*mongo.Client, context.Context,
+	context.CancelFunc, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(),
+		30*time.Second)
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	return client, ctx, cancel, err
+}
+
+func close(client *mongo.Client, ctx context.Context,
+	cancel context.CancelFunc) {
+
+	defer cancel()
+
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+}
 
 func gameRunner(path string, metafile string) {
 	cmd := exec.Command(filepath.Join(path, "run"), filepath.Join(path, metafile))
@@ -47,6 +73,12 @@ func main() {
 	// for _, metafile := range metaFiles {
 	// 	gameRunner(gamePath, metafile)
 	// }
+
+	client, ctx, cancel, err := connect("mongodb://localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+	defer close(client, ctx, cancel)
 
 	r := gin.Default()
 	r.MaxMultipartMemory = 8 << 20 //8Mib
